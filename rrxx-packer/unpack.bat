6f64d44783e70047b33f18ed96c256269dc89f19
Rumble Moses <118653911+rumblerosesxx@users.noreply.github.com>
2024-10-16 12:24:14 +0900

    Fix issue with paths with spaces

--- rrxx-packer/unpack.bat
+++ rrxx-packer/unpack.bat
@@ -1,6 +1,6 @@
 @echo off
 set PowerShellScriptPath=%~dp0resources\unpack.ps1
-set FilePath=%1
+set FilePath=%~1
 
 if "%FilePath%"=="" (
     echo Drag and drop a file onto this batch file.
@@ -8,6 +8,6 @@ if "%FilePath%"=="" (
     exit /b 1
 )
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File %PowerShellScriptPath% -file %FilePath%
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%PowerShellScriptPath%" -file "%FilePath%"
 pause
 
